803a74c55812001f7d1489d4e213ed0295a6c961
MoonTahoe <alex@moonhighway.com>
2019-02-01 16:27:51 -0800

    Lesson 10: Create a GraphQL Object Type and Query a list of Objects

--- api/index.js
+++ api/index.js
@@ -1,9 +1,33 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+    "An object that describes the details of a single ski day"
+    type SkiDay {
+        "A unique identifier for the record"
+        id: ID!
+        "The date that this ski day occurred"
+        date: String!
+        "The mountain where this ski day took place"
+        mountain: String!
+        "The conditions at the mountain on this particular ski day"
+        conditions: Conditions
+    }
+    "A choice of options for conditions on a ski day"
+    enum Conditions {
+        "Fresh snow, either deep snow or packed powder"
+        POWDER
+        "Heavy warm wet snow that makes it hard to turn"
+        HEAVY
+        "Hard bulletproof ice, scary conditions on steep slopes"
+        ICE
+        "A little bit of snow. a lot of rocks, bushes, and trees."
+        THIN
+    }
     type Query {
         "Count of total days skied during a season"
         totalDays: Int
+        "A list of all ski days that an individual has recorded this season"
+        allDays: [SkiDay!]!
     }
     type Mutation {
         "Adds a day to a skier's total number of ski days during a season"
@@ -13,11 +37,31 @@ const typeDefs = `
     }
 `;
 
-let skiDays = 0;
+let skiDays = [
+  {
+    id: "2WEKaVNO",
+    date: "3/28/2019",
+    mountain: "Mt. Tallac",
+    conditions: "POWDER"
+  },
+  {
+    id: "hwX6aOr7",
+    date: "1/2/2019",
+    mountain: "Freel Peak",
+    conditions: "POWDER"
+  },
+  {
+    id: "a4vhAoFG",
+    date: "11/23/2019",
+    mountain: "Tamarack Peak",
+    conditions: "ICE"
+  }
+];
 
 const resolvers = {
   Query: {
-    totalDays: () => skiDays
+    totalDays: () => skiDays.length,
+    allDays: () => skiDays
   },
   Mutation: {
     addDay: () => ++skiDays,
