cde1435b25349d412b2acabfd75d6bf6e51810a5
MoonTahoe <alex@moonhighway.com>
2019-02-01 16:23:29 -0800

    Lesson 8: Increment the Day Count with the Mutation Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React, { Fragment } from "react";
 import { gql } from "apollo-boost";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
 
 const COUNT_DAYS_QUERY = gql`
   query {
     totalDays
+  }
+`;
+
+const ADD_DAY_MUTATION = gql`
+  mutation add {
+    addDay
+  }
+`;
+
+const REMOVE_DAY_MUTATION = gql`
+  mutation remove {
+    removeDay
   }
 `;
 
@@ -17,6 +29,20 @@ const App = () => (
         return <p>total days: {data.totalDays}</p>;
       }}
     </Query>
+    <p>
+      <Mutation
+        mutation={ADD_DAY_MUTATION}
+        refetchQueries={[{ query: COUNT_DAYS_QUERY }]}
+      >
+        {addDay => <button onClick={addDay}>+</button>}
+      </Mutation>
+      <Mutation
+        mutation={REMOVE_DAY_MUTATION}
+        refetchQueries={[{ query: COUNT_DAYS_QUERY }]}
+      >
+        {removeDay => <button onClick={removeDay}>-</button>}
+      </Mutation>
+    </p>
   </Fragment>
 );
 
