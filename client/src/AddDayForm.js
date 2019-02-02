e6c2050fef3bafdf563f20ca8b472e9aca4457ec
MoonTahoe <alex@moonhighway.com>
2019-02-01 17:39:21 -0800

    Lesson 19: Use the Mutation Component with Input Arguments

--- client/src/AddDayForm.js
+++ client/src/AddDayForm.js
@@ -1,39 +1,70 @@
-import React, { Fragment, Component } from "react";
+import React, { Component } from "react";
 import { gql } from "apollo-boost";
 import { Mutation } from "react-apollo";
 
-const ADD_DAY_MUTATION = gql`
-  mutation add {
-    addDay(date: "", mountain: "") {
+const COUNT_DAYS_QUERY = gql`
+  query {
+    totalDays
+    allDays {
       id
+      date
+      mountain
+      conditions
+    }
+  }
+`;
+
+const ADD_DAY_MUTATION = gql`
+  mutation add($input: AddDayInput!) {
+    addDay(input: $input) {
+      id
+      date
+      mountain
+      conditions
     }
   }
 `;
 
 export default class AddDayForm extends Component {
   state = {
-    error: null
-  };
-  displayError = error => {
-    this.setState({ error });
+    mountain: "",
+    conditions: this.props.conditions[0],
+    date: new Date().toISOString().substring(0, 10)
   };
   render() {
     return (
-      <Fragment>
-        <select>
+      <form onSubmit={e => e.preventDefault()}>
+        <input
+          type="text"
+          value={this.state.mountain}
+          onChange={e => this.setState({ mountain: e.target.value })}
+          placeholder="mountain..."
+        />
+        <input
+          type="date"
+          value={this.state.date}
+          onChange={e => this.setState({ date: e.target.value })}
+        />
+        <select onChange={e => this.setState({ conditions: e.target.value })}>
           {this.props.conditions.map(c => (
             <option key={c} value={c}>
               {c.toLowerCase()}
             </option>
           ))}
         </select>
-        <Mutation mutation={ADD_DAY_MUTATION} onError={this.displayError}>
-          {mutation => <button onClick={mutation}>Add Day</button>}
+        <Mutation
+          mutation={ADD_DAY_MUTATION}
+          refetchQueries={[{ query: COUNT_DAYS_QUERY }]}
+        >
+          {mutation => (
+            <button
+              onClick={() => mutation({ variables: { input: this.state } })}
+            >
+              Add Day
+            </button>
+          )}
         </Mutation>
-        {this.state.error && (
-          <p style={{ color: "red" }}>{this.state.error.message}</p>
-        )}
-      </Fragment>
+      </form>
     );
   }
 }
