dfa295f4d3b83ccea8432e5b0497beee3528c1c1
MoonTahoe <alex@moonhighway.com>
2019-02-01 16:21:56 -0800

    Lesson 7: Change Data with GraphQL Mutations

--- api/index.js
+++ api/index.js
@@ -5,11 +5,23 @@ const typeDefs = `
         "Count of total days skied during a season"
         totalDays: Int
     }
+    type Mutation {
+        "Adds a day to a skier's total number of ski days during a season"
+        addDay: Int
+        "Removes a day from a skier's total number of ski days during a season"
+        removeDay: Int
+    }
 `;
+
+let skiDays = 0;
 
 const resolvers = {
   Query: {
-    totalDays: () => 100
+    totalDays: () => skiDays
+  },
+  Mutation: {
+    addDay: () => ++skiDays,
+    removeDay: () => --skiDays
   }
 };
 
